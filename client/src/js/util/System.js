1383b9f64b0f0f0ac13eaead3f4969ee04574eb8
mlmar <marcloussm@gmail.com>
2020-09-18 06:04:39 -0700

    vercel

--- client/src/js/util/System.js
+++ client/src/js/util/System.js
@@ -1,6 +1,6 @@
 /********* FOR DEVELOPMENT ONLY *********/
 
-const local_server = true;
+const local_server = false;
 const local_service = "http://localhost:3300/test";
-const deployed_service = "";
+const deployed_service = "https://twothousand-auth.vercel.app/test";
 export const SERVICE_URL = local_server ? local_service : deployed_service;
