cac9cd45c54c39446337067dd8630b5f792c7c83
mlmar <marcloussm@gmail.com>
2020-09-24 00:07:09 -0700

    updated project link

--- client/src/js/util/System.js
+++ client/src/js/util/System.js
@@ -2,5 +2,5 @@
 
 const local_server = false; // set this to true if using local api
 const local_service = "http://localhost:3300/test";
-const deployed_service = "https://twothousands-auth.vercel.app/test";
+const deployed_service = "https://twothousandz-auth.vercel.app/test";
 export const SERVICE_URL = local_server ? local_service : deployed_service;
